d95be09bc0027bb59829652bf7cf50b438941b04
Jon Evans <jle@foraker.com>
2013-02-05 23:34:25 -0700

    Finish representative search by zip code functionality

--- app/models/congress/representative_search.rb
+++ app/models/congress/representative_search.rb
@@ -10,16 +10,26 @@ module Congress
     end
 
     def results
-      response
+      raw_results.map { |attributes| Representative.new(attributes) }
     end
 
     private
+    def raw_results
+      response['results'].each do |result|
+        unwanted_attributes.each { |unwanted_attribute| result.delete(unwanted_attribute) }
+      end
+    end
+
     def response
       self.class.get(url)
     end
 
     def url
       "/legislators/locate?apikey=#{Settings.api_key}&zip=#{zip}"
+    end
+
+    def unwanted_attributes
+      ['state_name']
     end
   end
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,5 +2,33 @@ class Representative < ActiveRecord::Base
   belongs_to :state
   validates :name, :state, presence: true
 
-  attr_accessible :district, :leader, :name, :title, :url, :state
+  attr_accessible :district, :leader, :name, :title, :url, :state,
+    :bioguide_id, :birthday, :chamber, :contact_form, :crp_id,
+    :facebook_id, :fax, :fec_ids, :first_name, :gender, :govtrack_id,
+    :in_office, :last_name, :middle_name, :name_suffix, :nickname, :office,
+    :party, :phone, :state, :state_name, :thomas_id, :title, :twitter_id,
+    :votesmart_id, :website, :youtube_id, :lis_id, :senate_class, :state_rank
+
+  def bioguide_url
+    "http://bioguide.congress.gov/scripts/biodisplay.pl?index=#{bioguide_id}"
+  end
+
+  def facebook_url
+    "http://www.facebook.com/#{facebook_id}"
+  end
+
+  def twitter_url
+    "http://www.twitter.com/#{twitter_id}"
+  end
+
+  def state=(state)
+    case state
+    when String
+      self[:state] = State.find_by_abbreviation(state)
+    when State
+      self[:state] = state
+    else
+      raise "Unknown state: #{state.inspect}"
+    end
+  end
 end
